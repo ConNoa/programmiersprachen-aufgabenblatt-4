42868e18a79e1e998489dad44820eaf45c1134aa
ConNoa <constantin.noack@uni-weimar.de>
2017-06-06 21:00:13 +0200

    4.5 fertig

--- source/List.hpp
+++ source/List.hpp
@@ -33,33 +33,87 @@ struct ListNode
 };
 
 
-template <typename T>
-struct ListIterator
-{
-	
-
-
-	friend class List<T>;
-	
-	//not implemented yeat
-
-
-	private:
-		ListNode<T>* m_node = nullptr;
-};
-
 
 template <typename T>
 struct ListConstIterator
 {
 	friend class List<T>;
 
-public:
-	// not implemented yet
-private:
-	ListNode<T>* m_node = nullptr;
+		public:
+			// not implemented yet
+		private:
+			ListNode<T>* m_node = nullptr;
 };
+		
+template <typename T>
+struct ListIterator
+{
+	typedef ListIterator<T> Self;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef ptrdiff_t difference_type;
+	typedef std::forward_iterator_tag iterator_category;
 
+	friend class List<T>;
+
+	//Default Konstruktor
+	ListIterator():
+	m_node(nullptr){}
+
+	ListIterator(ListNode<T>* const&n):
+	m_node(n){}
+
+
+	reference operator*() const
+	{
+		return(m_node->m_value);
+	}
+
+	pointer operator->() const
+	{
+	return &(m_node->m_value);
+		}
+
+	Self& operator++()
+	{
+		*this = next(); 	
+		return *this; 
+	}
+
+	Self operator++(int)
+	{
+		Self tmp = *this;
+	    ++(*this);
+		return tmp;
+	}
+
+	bool operator== (const Self& x) const
+	{
+		return(m_node == x.m_node);
+	}
+
+	bool operator!=(const Self& x) const
+	{
+		return(m_node != x.m_node);
+	}
+
+	Self next() const
+	{
+		if (m_node)
+		{
+			return ListIterator(m_node->m_next);
+		}
+		else
+		{
+			return ListIterator(nullptr);
+		}
+	}
+
+		private:
+			//The Node the iterator is pointing to
+		ListNode<T>* m_node = nullptr;
+};
 
 template <typename T>
 class List
@@ -83,6 +137,7 @@ public:
 	m_size(0),
 	m_first(nullptr),
 	m_last(nullptr) {}
+//---------------------------------------------------------
 
 	//Leere Liste?
 	bool empty() const
@@ -95,6 +150,130 @@ public:
 	std::size_t size() const   
 	{
 		return(m_size);
+	}
+//---------------------------------------------------------
+
+	void push_front(T const& v)
+	{
+		if(m_size==0)//Liste ist leer
+		{
+			ListNode <T>* newfront = new ListNode<T>{v, nullptr, nullptr};
+			m_first = newfront;
+			m_last = newfront;
+		}
+		else
+		{
+			ListNode <T>* newfront = new ListNode<T>{v, nullptr, m_first};
+			m_first->m_prev=newfront;
+			m_first=newfront;
+		}
+
+		m_size++;
+
+	}
+
+	void push_back(T const& v)
+	{
+
+		if(m_size==0)//Liste ist leer
+		{
+			ListNode <T>* newback = new ListNode<T>{v, nullptr, nullptr};
+			m_first = newback;
+			m_last = newback;
+		}
+		else
+		{
+			ListNode <T>* newback = new ListNode<T>{v, m_last, nullptr};
+			m_last->m_next=newback;
+			m_last=newback;
+		}
+		m_size++;
+	}
+
+	void pop_front()
+	{
+		if(m_size<1)
+		{
+			throw std::out_of_range("List is empty, can´t pop");
+		}
+
+		if(m_size==1)
+		{
+
+			m_first=nullptr;
+			m_last=nullptr;
+			m_size--;
+		}
+
+		if(m_size>1)
+		{
+			m_first=m_first->m_next;
+			m_first->m_prev=nullptr;
+			m_size--;
+		}
+
+
+	}
+
+	void pop_back()
+	{
+		if(m_size<1)
+		{
+			throw std::out_of_range("List is empty, can´t pop");
+		}
+
+		if(m_size==1)
+		{
+
+			m_first=nullptr;
+			m_last=nullptr;
+			m_size--;
+		}
+
+		if(m_size>1)
+		{
+			m_last=m_last->m_prev;
+			m_last->m_next=nullptr;
+			m_size--;
+		}
+
+
+	}
+//---------------------------------------------------------
+//--------------Warum so in der Form?----------------------
+//---------------------------------------------------------
+	
+	T const& front() const
+  	{
+   		return m_first->m_value;
+  	}
+
+
+  	T const& back() const
+  	{
+    	return m_last->m_value;
+  	}
+
+
+  	T& front()
+  	{
+    	return m_first->m_value;
+  	}
+
+
+  	T& back()
+  	{
+    	return m_last->m_value;
+	}
+//---------------------------------------------------------
+
+
+	void clear()
+  	{
+    	while(!empty())
+    	{
+      		pop_back();
+    	}
 	}
 
 private:
--- source/TestList.cpp
+++ source/TestList.cpp
@@ -3,25 +3,43 @@
 #include "List.hpp"
 
 
-TEST_CASE("Aufgabe 4.2", "[test empty]")
+TEST_CASE("Aufgabe 4.2 [test empty test size]")
 {
 		List <float> tl;
 		REQUIRE (tl.empty());
-
-
-
+		REQUIRE (tl.size()==0);
 }
 
-TEST_CASE("Aufgabe 4.2", "[test size]")
+
+TEST_CASE("Aufgabe 4.3 [push front push back pop front]")
 {
 		List <float> tl;
 		REQUIRE (tl.size()==0);
-		
-
+		tl.push_front(42);
+		tl.push_front(72);
+		tl.push_front(42);
+		REQUIRE (tl.size()==3);
+		tl.push_back(24);
+		REQUIRE (tl.size()==4);
+		tl.pop_front();
+		REQUIRE (tl.size()==3);
+		tl.pop_back();
+		REQUIRE (tl.size()==2);
+		REQUIRE(72 == tl.front());
+		REQUIRE(42 == tl.back());
 
 }
 
-
+TEST_CASE("should be empty after clearing", "[modifiers]")
+{
+	List<int> list; 
+	list.push_front(1); 
+	list.push_front(2); 
+	list.push_front(3); 
+	list.push_front(4); 
+	list.clear(); 
+	REQUIRE(list.empty());
+}
 
 int main(int argc, char* argv[])
 {
